2868053dfa876750ba0616ec439d1efd26040cb5
nikogenix <nikogenix@gmail.com>
2023-06-23 04:06:44 +0300

    Testing Watchtower

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+	res.send("<h1>Hello World v2</h1>");
+});
 
-const PORT = 8080
+const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+	console.log(`Server running on port ${PORT}`);
+});
